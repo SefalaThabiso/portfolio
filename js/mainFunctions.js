6496c820bc4ecdbb73c151e2832d9b722a7a58f9
SefalaThabiso <33560898+SefalaThabiso@users.noreply.github.com>
2023-01-13 12:47:59 +0200

    Update mainFunctions.js

--- js/mainFunctions.js
+++ js/mainFunctions.js
@@ -1,5 +1,5 @@
 $(document).ready(() => {
-  $(".navbar_wraper").load("/components/navbar.html");
+  $(".navbar_wraper").load("../components/navbar.html");
   $(".about__me_wraper").load("/components/about__me.html");
   $(".tech__stack_wraper").load("/components/tech__stack.html");
   $(".projects_wraper").load("/components/projects.html");
