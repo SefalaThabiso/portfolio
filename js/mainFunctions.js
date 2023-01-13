07b346e67acb03e627adf615e694a96790ed6870
SefalaThabiso <33560898+SefalaThabiso@users.noreply.github.com>
2023-01-13 12:54:32 +0200

    Update mainFunctions.js

--- js/mainFunctions.js
+++ js/mainFunctions.js
@@ -1,7 +1,8 @@
 $(document).ready(() => {
+  console.log("ready");
   $(".navbar_wraper").load("../components/navbar.html");
   $(".about__me_wraper").load("../components/about__me.html");
   $(".tech__stack_wraper").load("../components/tech__stack.html");
   $(".projects_wraper").load("../components/projects.html");
-  $(".footer_wraper").load("../components/footer.html");
+  $(".footer_wraper").load("../gitcomponents/footer.html");
 });
